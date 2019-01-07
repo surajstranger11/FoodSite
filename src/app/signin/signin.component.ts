fb8c3a851bc5888a9eafc8d05008a73f6053816e
Suraj Singh <surajstranger11@gmail.com>
2019-01-07 18:32:26 +0530

    == fixed

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -16,9 +16,9 @@ export class SigninComponent implements OnInit {
 
   public socialSignIn(socialPlatform: string) {
     let socialPlatformProvider;
-    if(socialPlatform == 'facebook') {
+    if(socialPlatform === 'facebook') {
       socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    } else if(socialPlatform == 'google') {
+    } else if(socialPlatform === 'google') {
       socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
     }
 
